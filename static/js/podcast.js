63cfd079a217d9e07fc9ac99fa5bfbdd01a06fab
Timothy Hu <hutimk@gmail.com>
2022-10-18 16:18:31 -0700

    commit

--- static/js/podcast.js
+++ static/js/podcast.js
@@ -1,5 +1,5 @@
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 3000;
+let interval = 3200;
 valueDisplays.forEach((valueDisplay) => {
     let startValue = 0;
     let endValue = parseInt(valueDisplay.getAttribute("data-val"));
